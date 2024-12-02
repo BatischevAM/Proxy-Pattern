138d6cda74652410ff9092802f979b5d50f2a290
BatischevAM <batischevam@mer.ci.nsu.ru>
2024-12-02 10:29:15 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using Proxy_Pattern;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Proxy proxy = new Proxy(TimeSpan.FromSeconds(10));
+        Console.WriteLine(proxy.Request("Запрос 1"));
+        Console.WriteLine(proxy.Request("Запрос 1"));
+        Thread.Sleep(9000);
+        Console.WriteLine(proxy.Request("Запрос 1"));
+    }
+}
